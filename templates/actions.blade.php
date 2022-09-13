12463719ac6f1d496613dff5167b7abbd7bada5b
Maher El Gamil <maherelgamil@users.noreply.github.com>
2022-09-13 22:59:07 +0200

    fix plural Model Names

--- templates/actions.blade.php
+++ templates/actions.blade.php
@@ -4,25 +4,27 @@ namespace App\Filament\Resources\{{ $resource }}\Pages;
 
 use App\Filament\Resources\{{ $resource }};
 use Filament\Resources\Pages\ListRecords;
-use Filament\Pages\Actions\ButtonAction;
-use App\Exports\{{ $model }}sExport;
+use Filament\Pages\Actions\Action;
+use App\Exports\{{ $pluralModelName }}Export;
 use Maatwebsite\Excel\Facades\Excel;
 use App\Models\{{ $model }};
 use Filament\Forms;
 
-class List{{ $model }}s extends ListRecords
+class List{{ $pluralModelName }} extends ListRecords
 {
     protected static string $resource = {{ $resource }}::class;
 
     protected function getActions(): array
     {
         return array_merge(parent::getActions(), [
-            ButtonAction::make('export')->action('export'),
+            Action::make('export')
+                ->button()
+                ->action('export'),
         ]);
     }
 
     public function export()
     {
-        return Excel::download(new {{ $model }}sExport, '{{ Str::lower($model) }}s.xlsx');
+        return Excel::download(new {{ $pluralModelName }}Export, '{{ Str::lower($pluralModelName) }}.xlsx');
     }
 }
